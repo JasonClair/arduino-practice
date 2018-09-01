53a108ff62b59582d3b00feca8242a2c1f22dfb5
Jason <git@jason-clair.co.uk>
2018-09-01 17:05:12 +0100

    Added Sound Monitor

--- Light Dependent Resistor/Light Dependent Resistor/Light Dependent Resistor.ino
+++ Light Dependent Resistor/Light Dependent Resistor/Light Dependent Resistor.ino
@@ -1,9 +1,12 @@
 //code from https://www.instructables.com/id/Arduino-Light-Sensitive-Bar-Graph/
-
+int soundSensor = 2;
 const int NbrLEDs = 6;
 const int ledPins[] = { 13, 12, 11, 10,9, 8 };
 const int analogInPin = 0;  // Analog input pin connected to variable resistor
 const int wait = 30;
+
+bool enable = false;
+unsigned long soundDetectedTime;
 
 // Swap values of the following two constants if cathodes are connected to Gnd
 const boolean LED_ON = LOW;
@@ -14,6 +17,7 @@ int ledLevel = 0;           // sensor value converted into LED 'bars'
 
 void setup() {
 	Serial.begin(9600);
+	pinMode(soundSensor, INPUT);
 	for (int led = 0; led < NbrLEDs; led++)
 	{
 		pinMode(ledPins[led], OUTPUT);                             // make all the LED pins outputs
@@ -21,17 +25,41 @@ void setup() {
 }
 
 void loop() {
-	sensorValue = analogRead(analogInPin);                 // read the analog in value
-	ledLevel = map(sensorValue, 0, 700, 0, NbrLEDs);  // map to the number of LEDs
+	int soundSensorValue = digitalRead(soundSensor);
+	if (soundSensorValue == 1) {
+		shouldLEDShow();
+	}
 
-	for (int led = 0; led < NbrLEDs; led++)
-	{
-		Serial.println(ledLevel);
-		if (led < ledLevel) {
-			digitalWrite(ledPins[led], LED_ON);                    // turn on pins less than the level
+
+	if (enable) {
+		sensorValue = analogRead(analogInPin);                 // read the analog in value
+		ledLevel = map(sensorValue, 0, 700, 0, NbrLEDs);  // map to the number of LEDs
+
+		for (int led = 0; led < NbrLEDs; led++)
+		{
+			//Serial.println(ledLevel);
+			if (led < ledLevel) {
+				digitalWrite(ledPins[led], LED_ON);                    // turn on pins less than the level
+			}
+			else {
+				digitalWrite(ledPins[led], LED_OFF);                 // turn off pins higher than the level
+			}
+		}
+	}
+	else {
+		for (int led = 0; led < NbrLEDs; led++) {
+			digitalWrite(ledPins[led], LED_OFF);
+		}
+	}
+}
+
+void shouldLEDShow() {
+	if (millis() - soundDetectedTime > 1000) {
+		if (enable) {
+			enable = false;
 		}
 		else {
-			digitalWrite(ledPins[led], LED_OFF);                 // turn off pins higher than the level
+			enable = true;
 		}
 	}
 }
